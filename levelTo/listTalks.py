9aa865e69e1deab7dcd3e963346669732343779a
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-10 13:48:15 +0530

    Second commit data types

---
+++ levelTo/listTalks.py
@@ -0,0 +1,13 @@
+lst1 = ["john", "lesnar", "taker"]
+lst2 = ["henry", "michaels"]
+# print(lst1[1], lst2[0])
+# del lst1[0]
+# print(lst1[0], lst1, lst2)
+lst1.append(3)
+lst1.insert(2, "sanjay")
+# lst1.remove("sanjay")
+lst1.insert(2, "sanjay")
+print(lst1.count("sanjay"))
+print(lst1 + lst2)
+
+
