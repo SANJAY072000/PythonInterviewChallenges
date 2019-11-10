7629501f85a2ca39694ef7839274436d034720af
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-10 14:14:48 +0530

    Dictionary talks

---
+++ levelTo/dictionaryTalks.py
@@ -0,0 +1,17 @@
+dict1 = {
+    "spy": 80,
+    "iron": 90,
+    "thor": 95
+}
+dict2 = {
+    "bat": 99,
+    "flash": 70,
+    "acqua": 85,
+}
+lst = [1, 2, 3, 4, 5, 6, 7]
+# dict1.clear()
+dt = dict.fromkeys(lst)
+print(dt)
+
+
+
