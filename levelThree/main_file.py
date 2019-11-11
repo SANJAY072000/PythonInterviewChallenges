b199f572d4cb40647a277c9423dc5af41f58edcc
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-11 16:30:45 +0530

    Done with challenge of calculator

--- levelThree/main_file.py
+++ levelThree/main_file.py
@@ -1,7 +1,5 @@
-# import sanjay
 from sanjay import *
-print(am(3, 5))
-print(sm(3, 5))
+mnu()
 
 
 
--- levelThree/sanjay.py
+++ levelThree/sanjay.py
@@ -1,7 +1,18 @@
+from calcfun import *
+
+
 def mnu():
     print("\n 1. Add\n 2. Subtract\n 3. Multiply")
     ch = int(input("\n Enter your choice : "))
     if ch == 1:
+        print(add())
+    elif ch == 2:
+        print(sub())
+    elif ch == 3:
+        print(mul())
+    else:
+        print("\n Wrong choice")
+
 
 
 
