6dac62af4bb813c66f1497c9a0f28b86fe4b5dda
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-11 16:57:51 +0530

    Found the day assignment done

--- levelThree/myDate.py
+++ levelThree/myDate.py
@@ -1,4 +1,15 @@
-import time
-print(time.asctime(time.localtime(time.time())))
-
+import calendar
+# print(time.asctime(time.localtime(time.time())))
+day = {
+    0: "Monday",
+    1: "Tuesday",
+    2: "Wednesday",
+    3: "Thursday",
+    4: "Friday",
+    5: "Saturday",
+    6: "Sunday"
+}
+a = input("Enter date : ")
+a = a.split('-')
+print(day[calendar.weekday(int(a[2]), int(a[1]), int(a[0]))])
 
