a07b33beeb58621701bae5aaadb5f859b205378c
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-11 15:13:58 +0530

    Done with some updates

--- levelThree/functionsOne.py
+++ levelThree/functionsOne.py
@@ -1,18 +1,18 @@
-# print("Hello world")
-# def sanjay(a):
-#     print("My function")
-#     print(a)
-# sanjay("Sanjay Singh Bisht")
+print("Hello world")
+def sanjay(a):
+    print("My function")
+    print(a)
+sanjay("Sanjay Singh Bisht")
 
 
-def cret(n, e="djfjgjgj", ph):
+def cret(n, e="djfjgjgj", ph=456):
     print("Name : %s, email : %s, phone : %d" %(n, e, ph))
 
 
-# a = int(input())
-# b = input()
-# # c = input()
-# cret(b, a)
+a = int(input())
+b = input()
+c = input()
+cret(b)
 
 
 
