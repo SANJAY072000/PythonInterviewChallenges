b47a959762593cfa0f91cf9043213f8643aba2d1
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-11 15:31:24 +0530

    Testing done

--- levelThree/multiArgs.py
+++ levelThree/multiArgs.py
@@ -1,12 +1,15 @@
-def add(*n):
-    s = 0
-    for i in n:
-        s += i
-    return s
+# def add(*n):
+#     s = 0
+#     for i in n:
+#         s += i
+#     return s
+#
+#
+# print(add(2, 3, 4, 5, 6))
 
 
-print(add(2, 3, 4, 5, 6))
-
-
-
+l = ['456', 2, 3, 4, 5, 6]
+print(l)
+del l[0]
+print(l)
 
