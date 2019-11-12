ea4372cc385f8b73367e5a9d470772341980eba6
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-12 20:16:59 +0530

    Exception handling done

--- levelThree/myFiles.py
+++ levelThree/myFiles.py
@@ -1,16 +1,19 @@
-def main():
+# def main():
     # file = open("lco.txt", "a+")
     # for i in range(20):
     #     file.write("Python course going on\n")
     # file.close()
+# file = open("lco.txt", "r")
+# if file.mode == 'r':
+#     filecontent = file.read()
+#     print(filecontent)
+try:
     file = open("lco.txt", "r")
-    if file.mode == 'r':
-        filecontent = file.read()
-        print(filecontent)
+    print("Success in reading")
+except IOError:
+    print("File does not exist")
 
 
 
-if __name__ == "__main__":
-    main()
 
 
