d13c73b4106bbc0f1e53e03cbfdf8ba2abf8fc61
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-25 21:06:09 +0530

    getters and setters

--- levelFour/oopsOne.py
+++ levelFour/oopsOne.py
@@ -1,13 +1,19 @@
 class Phone:
     name = "S10"
     brand = "Samsung"
+    cv = "Sanjay"
+    def get_cv(self):
+        return self.cv
+    def set_cv(self, nn, sn):
+        self.cv = nn + sn
     def BootLogo(self):
         print(self.name, "Samsung")
-    def __init__(self, a):
-        self.name = a
 
-ph = Phone("S35")
-ph.name = "s79"
-ph.BootLogo()
-
-
+ph = Phone()
+# ph.name = "s79"
+# ph.BootLogo()
+print(ph.cv)
+print(ph.get_cv())
+ph.set_cv("Sumit", "Bisht")
+print(ph.cv)
+print(ph.get_cv())
