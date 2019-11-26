42cf40faf91f9a0d881a9d8a597656bbd59c702e
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-26 17:13:06 +0530

    helper file part 1

--- todoApp/db_helper.py
+++ todoApp/db_helper.py
@@ -1,7 +1,29 @@
 import sqlite3
+db = 'my_todo.db'
+table = 'task'
+conn = sqlite3.connect(db)
+c = conn.cursor()
+
+def create_table():
+    sql = 'CREATE TABLE IF NOT EXISTS ' + table \
+    + ' (ID INTEGER PRIMARY KEY AUTOINCREMENT, TASKNAME TEXT)'
+    c.excute(sql)
+
+def data_entry(task):
+    pass
+
+def print_data():
+    pass
+
+def delete_task():
+    pass
+
+def close_cursor():
+    c.close()
+    conn.close()
 
 
-
+    
 
 
 
