a49b43583ddec971d2076c112aaccb3a7860968e
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-28 17:58:24 +0530

    All functions in python

--- levelFive/set_map.py
+++ levelFive/set_map.py
@@ -1,4 +1,10 @@
-st = [1, 2, 4, 3, 3.0]
-sq = lambda num: num*num
-print(set(map(sq, st)))
+# st = [1, 2, 4, 3, 3.0]
+# sq = lambda num: num*num
+# print(set(map(sq, st)))
+
+
+ml = [12, -78, 23, 23]
+print(all(ml))
+
+
 
