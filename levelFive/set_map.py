5ea9997efdf475340cf38d3526142897a73d41f1
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-28 18:49:41 +0530

    Negative indexing

--- levelFive/set_map.py
+++ levelFive/set_map.py
@@ -1,14 +1,11 @@
 # st = [1, 2, 4, 3, 3.0]
 # sq = lambda num: num*num
 # print(set(map(sq, st)))
-
-
-ml = [114, 0]
-print(any(ml))
-
-# ex = ()
-# if ex:
-#     print(True)
-# else:
-#     print(False)
+# ml = [114, 0]
+# print(all(ml))
+# ml = [114, 0]
+# print(any(ml))
+a = "Sanjay Singh Bisht"
+print(a[::-1])
+# print(list(a))
 
