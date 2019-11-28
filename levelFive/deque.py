e88814b66f39d0b9fdf36e20de0ebe718e66c5aa
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-11-28 18:59:49 +0530

    collections and deque

--- levelFive/deque.py
+++ levelFive/deque.py
@@ -1,4 +1,11 @@
 from collections import *
-
+from string import *
+dy = deque(ascii_lowercase)
+# print(dy)
+# for i in dy:
+#     print(i, end=',')
+# print(len(dy))
+print(dy.appendleft("111"))
+print(dy)
 
 
