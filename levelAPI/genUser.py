dc00ad52dfe14cc78041e74416556be480399710
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-12-03 20:25:14 +0530

    Assg done

--- levelAPI/genUser.py
+++ levelAPI/genUser.py
@@ -12,13 +12,13 @@ def get_data(url):
 def load_json(data):
     if not data is None:
         j = json.loads(data)
-        return [j["results"][0]["name"]["first"], j["results"][0]["name"]["last"], j["results"][0]["email"]]
+        return [j["results"][0]["name"]["title"] + ". " + j["results"][0]["name"]["first"] + j["results"][0]["name"]["last"], j["results"][0]["email"], j["results"][0]["gender"], j["results"][0]["dob"]["date"]]
 
 
 url = "https://randomuser.me/api"
 values = load_json(get_data(url))
 if values:
-    print("First Name : %s\nLast Name : %s\nEmail is : %s" %(values[0], values[1], values[2]))
+    print("Name : %s\nEmail : %s\nGender : %s\nDOB : %s" %(values[0], values[1], values[2], values[3].split('T')[0]))
 
 
 
