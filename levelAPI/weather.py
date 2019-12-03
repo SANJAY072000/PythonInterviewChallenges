c7b945915934e7fc4637a3bc939811fe4ac0f360
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-12-03 20:38:42 +0530

    Weather api fetched successfully

--- levelAPI/weather.py
+++ levelAPI/weather.py
@@ -1,4 +1,11 @@
+from json import *
+from requests import *
 
+
+a = lambda u: str(loads(get(u).text)["main"]["temp"]) + " F"
+
+
+print("\n In London, temperature is ", a("https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b6907d289e10d714a6e88b30761fae22"))
 
 
 
